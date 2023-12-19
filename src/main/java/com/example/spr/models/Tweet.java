5e2f932215118172dbe0846cf8175abbda6295e0
Al <kovchi20@mail.ru>
2023-12-19 20:36:06 +0300

    post1

---
+++ src/main/java/com/example/spr/models/Tweet.java
@@ -0,0 +1,14 @@
+package com.example.spr.models;
+
+
+public class Tweet {
+    String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
